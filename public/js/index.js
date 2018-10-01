a090606f287a6bb2bb1d4c04a07149a427a44790
pratiksha kapadne <pratikshakapadne52@gmail.com>
2018-10-01 21:01:22 +0530

    emit new message on createmessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect',function ()  {
   console.log('connected to server');
 
-socket.emit('createMessage',{
-  from:'pratiksha',
-  text:'yup,that works for me'
-});
 });
 socket.on('disconnect',function () {
   console.log('disconnected from server');
